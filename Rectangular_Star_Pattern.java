4697c30f7c0f2567db35ace37d12f1991b3269ae
Guntaka Meghana Reddy <meghanareddyg503@gmail.com>
2026-09-16 20:49:42 +0530

    Pattern Problems prob 1 done

---
+++ Rectangular_Star_Pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Rectangular_Star_Pattern {
+    public static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);    
+        pattern(sc.nextInt());
+        sc.close();
+    }
+}
