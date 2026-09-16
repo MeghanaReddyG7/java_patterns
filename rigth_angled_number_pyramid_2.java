fe4f7252f043c5adaea20154f9585c2398429491
Guntaka Meghana Reddy <meghanareddyg503@gmail.com>
2026-09-16 21:37:26 +0530

    number pyramid 2

---
+++ rigth_angled_number_pyramid_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class rigth_angled_number_pyramid_2 {
+    public static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        pattern(sc.nextInt());
+        sc.close();
+
+    }
+}
