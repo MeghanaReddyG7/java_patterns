824068ddd23f1bc4a48e7dd3f16159433c3a3e1d
Guntaka Meghana Reddy <meghanareddyg503@gmail.com>
2026-09-17 12:22:24 +0530

    test cases added

--- Inverted_Numbered_Right_Pyramid.java
+++ Inverted_Numbered_Right_Pyramid.java
@@ -13,7 +13,14 @@ public class Inverted_Numbered_Right_Pyramid {
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        pattern(sc.nextInt());
+        System.out.println("Enter number of test cases: ");
+        int testcases=sc.nextInt();
+        for(int i=0;i<testcases;i++){     
+        int n=sc.nextInt();                         
+        System.out.println("for " + (i+1) + " test case pattern is below!"); 
+        System.out.println();
+        pattern(n);
+        }
         sc.close();
     }
 }
