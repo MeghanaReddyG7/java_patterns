b0fec30bcbac3fb892c1dce91dd2b31799ebb2e8
Guntaka Meghana Reddy <meghanareddyg503@gmail.com>
2026-09-17 23:08:18 +0530

    modiied with simpler  logic

--- increasing_letter_triangle.java
+++ increasing_letter_triangle.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class increasing_letter_triangle {
     public static void pattern(int n){
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<=i;j++){  //for(char ch='A';ch<='A'+i;ch++){ sout(ch)}
                 char letter=(char)(j-1+'A');
                 System.out.print(letter);
             }
